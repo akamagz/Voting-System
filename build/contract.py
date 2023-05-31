a2ad5fd1028b3629586a94bbaa0e717b890e962a
akamagz <122532627+akamagz@users.noreply.github.com>
2023-05-31 14:49:57 +0800

    Update contract.py

--- build/contract.py
+++ build/contract.py
@@ -4,46 +4,63 @@ from pyteal import *
 def approval():
     handle_creation = Seq(
         App.globalPut(Bytes("Count1"), Int(0)),
-        App.globalPut(Bytes("Count2"), Int(1)),
-        App.globalPut(Bytes("Count3"), Int(2)),
+        App.globalPut(Bytes("Count2"), Int(0)),
+        App.globalPut(Bytes("Count3"), Int(0)),
         Return(Int(1))
     )
 
-    handle_optin = Return(Int(0))
+    handle_optin = Return(Int(1))
 
-    handle_closeout = Return(Int(0))
+    handle_closeout = Return(Int(1))
 
     handle_updateapp = Return(Int(0))
 
     handle_deleteapp = Return(Int(0))
 
     scratchCount = ScratchVar(TealType.uint64)
+    voted = App.localGetEx(Int(0), App.id(), Bytes("voted"))
+    voted_accounts = App.globalGetEx(Int(0), Bytes("voted_accounts"))
 
+    def add_count(count_key):
+        return Seq(
+            scratchCount.store(App.globalGet(count_key)),
+            App.globalPut(count_key, scratchCount.load() + Int(1)),
+            Return(Int(1))
+        )
 
-    add1 = Seq(
-        scratchCount.store(App.globalGet(Bytes("Count1"))), 
-        App.globalPut(Bytes("Count1"), scratchCount.load() + Int(1)),
-        Return(Int(1))
-    )
+    def account_has_voted():
+        return Seq(
+            voted_accounts,
+            If(
+                voted_accounts.hasValue(),
+                Return(Int(0))
+            )
+        )
 
-    add2 = Seq(
-        scratchCount.store(App.globalGet(Bytes("Count2"))), 
-        App.globalPut(Bytes("Count2"), scratchCount.load() + Int(1)),
-        Return(Int(1))
-    )
-
-    add3 = Seq(
-        scratchCount.store(App.globalGet(Bytes("Count3"))), 
-        App.globalPut(Bytes("Count3"), scratchCount.load() + Int(1)),
-        Return(Int(1))
+    check_vote = Seq(
+        [
+            voted,
+            If(voted.hasValue(), Return(Int(0))),
+            account_has_voted(),
+            If(voted_accounts.hasValue(), Return(Int(0))),
+            App.localPut(Int(0), Bytes("voted"), Int(1)),
+            App.globalPut(Bytes("voted_accounts"), Int(1)),
+            Seq(
+                add_count(Bytes("Count1")),
+                add_count(Bytes("Count2")),
+                add_count(Bytes("Count3")),
+                Return(Int(1))
+            ),
+        ]
     )
 
     handle_noop = Seq(
         Assert(Global.group_size() == Int(1)),
         Cond(
-            [Txn.application_args[0] == Bytes("Add1"), add1], 
-            [Txn.application_args[0] == Bytes("Add2"), add2],
-            [Txn.application_args[0] == Bytes("Add3"), add3]
+            [Txn.application_args[0] == Bytes("Add1"), add_count(Bytes("Count1"))],
+            [Txn.application_args[0] == Bytes("Add2"), add_count(Bytes("Count2"))],
+            [Txn.application_args[0] == Bytes("Add3"), add_count(Bytes("Count3"))],
+            [Txn.application_args[0] == Bytes("vote"), Seq(check_vote)]
         )
     )
 
@@ -68,3 +85,4 @@ appFile.close()
 clearFile = open('clear.teal', 'w')
 clearFile.write(clear())
 clearFile.close()
+
